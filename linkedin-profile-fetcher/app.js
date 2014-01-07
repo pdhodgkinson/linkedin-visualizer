a2eb23e327af7a6cb918dca1fcdcf1233938f704
Peter Hodgkinson <peter@peterhodgkinson.com>
2014-01-07 10:36:45 -0500

    Fixes for Windows test

--- linkedin-profile-fetcher/app.js
+++ linkedin-profile-fetcher/app.js
@@ -1,33 +1,32 @@
-
 /**
  * Module dependencies.
  */
 
-  //id = 63DO4DUYsc
+//id = 63DO4DUYsc
 
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-    , api = require('./routes/api')
-  , company= require('./routes/company')
-  , http = require('http')
-  , path = require('path')
-    , auth = require('connect-auth'),
-    OAuth = require("oauth").OAuth,
-    liOauth = require("./linkedin_oauth"),
-  db = require("./db");
+var express = require('express'),
+    routes = require('./routes'),
+    user = require('./routes/user'),
+    api = require('./routes/api'),
+    company = require('./routes/company'),
+    http = require('http'),
+    path = require('path'),
+    auth = require('connect-auth'),
+    OAuth = require('oauth').OAuth,
+    liOauth = require('./linkedin_oauth');
 
-var myOauth = new liOauth('test', 'this', 'out');
-var example_keys= require('./linkedin_config');
-for(var key in example_keys) {
-    global[key]= example_keys[key];
+//var myOauth = new liOauth('test', 'this', 'out');
+//Todo: This is bad way to load a global config
+var exampleKeys = require('./linkedin_config');
+for (var key in exampleKeys) {
+    global[key] = exampleKeys[key];
 }
 
 var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3003);
-linkedinCallback= "http://localhost:" + app.get('port') + "/auth/linkedin_callback";
+var linkedinCallback = 'http://localhost:' + app.get('port') + '/auth/linkedin_callback';
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
@@ -39,53 +38,55 @@ app.use(express.session());
 app.use(auth({strategies: auth.Linkedin({consumerKey: linkedinConsumerKey, consumerSecret: linkedinConsumerSecret, callback: linkedinCallback}),
     trace: true,
     logoutHandler: require('connect-auth/lib/events').redirectOnLogout("/")}));
-app.use(function(req, res, next) {
-    if(req.isAuthenticated()) {
+app.use(function (req, res, next) {
+    if (req.isAuthenticated()) {
+        //TODO: figure out proper config for pre-configured values
         linkedinToken = req.getAuthDetails().linkedin_oauth_token;
         linkedinSecret = req.getAuthDetails().linkedin_oauth_token_secret;
     }
 
 
-    if (linkedinToken !== undefined && linkedinSecret !== undefined) {
-        console.log("yep authenticated");
-        console.log("linkedin_token: " + linkedinToken);
-        console.log("linkedin_secret: " + linkedinSecret);
+    if (typeof linkedinToken !== 'undefined' && typeof linkedinSecret !== 'undefined') {
+        //console.log('yep authenticated');
+        console.log('linkedin_token: ' + linkedinToken);
+        console.log('linkedin_secret: ' + linkedinSecret);
         req.linkedInOAuth = new liOauth(linkedinConsumerKey,
             linkedinConsumerSecret,
             linkedinToken,
             linkedinSecret
         );
 
-      if (typeof linkedinId !== 'undefined') {
-        //set linked in ID if stored in config
-        req.session.linkedInId = linkedinId;
-      }
+        if (typeof linkedinId !== 'undefined') {
+            //set linked in ID if stored in config
+            req.session.linkedInId = linkedinId;
+        }
 
         if (typeof req.session.linkedInId === 'undefined') {
-          console.log("Get user linkedinID");
-          req.linkedInOAuth.api.id(function(data) {
-            data = JSON.parse(data);
-            req.session.linkedInId = data.id;
-            console.log("Set linkedinID to: " + data.id);
-            next();
-          });
+            console.log('Get user linkedinID');
+            req.linkedInOAuth.api.id(function (data) {
+                data = JSON.parse(data);
+                req.session.linkedInId = data.id;
+                console.log('Set linkedinID to: ' + data.id);
+                next();
+            });
         } else {
-          next();
+            next();
         }
     } else {
-      console.log("not authenticated yet")
-      next();
+        console.log('not authenticated yet');
+        next();
     }
 });
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+if ('development' === app.get('env')) {
+    app.use(express.errorHandler());
 }
 
-var oa = new OAuth(
+/*
+ var oa = new OAuth(
     "https://api.linkedin.com/uas/oauth/requestToken"
     , "https://api.linkedin.com/uas/oauth/accessToken"
     , linkedinConsumerKey
@@ -94,6 +95,7 @@ var oa = new OAuth(
     , linkedinCallback
     , "HMAC-SHA1"
 );
+ */
 
 app.get('/', routes.index);
 app.get('/api/people', api.people);
@@ -101,23 +103,26 @@ app.get('/api/people', api.people);
 app.get('/user', user.index);
 app.get('/user/details', user.detail);
 app.get('/company/:id', company.details);
-app.get('/login', function(req, res) {
-    if( req.isAuthenticated()) {
+app.get('/login', function (req, res) {
+    'use strict';
+    if (req.isAuthenticated()) {
         res.redirect('/');
         res.end();
 
     } else {
-        req.authenticate('linkedin', function(error, authenticated) {
-        if( error ) {
-            // Something has gone awry, behave as you wish.
-            console.log( error );
-            res.end();
-        }
+        req.authenticate('linkedin', function (error, authenticated) {
+            console.log(authenticated);
+            if (error) {
+                // Something has gone awry, behave as you wish.
+                console.log(error);
+                res.end();
+            }
 
-});
+        });
     }
 });
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get('port'), function () {
+    'use strict';
+    console.log('Express server listening on port ' + app.get('port'));
 });
--- linkedin-profile-fetcher/linkedin_config.js
+++ linkedin-profile-fetcher/linkedin_config.js
@@ -1,5 +1,9 @@
-exports.linkedinConsumerKey= "y1utulntmqak";
-exports.linkedinConsumerSecret= "BUFdef1k2dNQTHyQ";
-exports.linkedinToken = "0827ee0f-5660-4a93-8073-5cf000d5b654";
-exports.linkedinSecret = "c303bdf3-8980-4729-b609-15fe09e0344f";
+exports.linkedinConsumerKey = 'y1utulntmqak';
+exports.linkedinConsumerSecret = 'BUFdef1k2dNQTHyQ';
+exports.linkedinToken = '0827ee0f-5660-4a93-8073-5cf000d5b654';
+exports.linkedinSecret = 'c303bdf3-8980-4729-b609-15fe09e0344f';
+/*
+ linkedin_token: 0827ee0f-5660-4a93-8073-5cf000d5b654
+ linkedin_secret: c303bdf3-8980-4729-b609-15fe09e0344f
+ */
 //exports.linkedinId = "63DO4DUYsc";
